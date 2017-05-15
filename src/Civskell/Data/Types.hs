6d25c122bf232fc700dd58bcd53be2e658ed8b35
Lazersmoke <lazersmoke@gmail.com>
2017-05-15 13:15:43 +0000

    Added lots of documentation

--- src/Civskell/Data/Types.hs
+++ src/Civskell/Data/Types.hs
@@ -23,7 +23,7 @@
 -- TODO: This export list is pretty scary. We should make tiered modules or something
 module Civskell.Data.Types
   -- Constants
-  (airChunk,allCoords,protocolVersion
+  (airChunk,allCoords,protocolVersion,legacyHandshakePacketConstant
   -- Helper functions
   ,blockInChunk,blockToChunk,blockToRelative,blockOnSide,showPacket,jsonyText,withLength,withListLength,indentedHex,serOpt,comb
   -- Type synonyms
@@ -107,19 +107,99 @@ import qualified Data.Serialize as Ser
 import qualified Data.Set as Set
 import qualified Data.Text as Text
 
+-- The current Minecraft Protocol Version number. This should be replaced with a
+-- configuration option, or even multiversion support in the future, but right
+-- now we only support 316, which is 1.11.2 Vanilla.
 protocolVersion :: Integer
 protocolVersion = 316
 
--- Type Synonyms
+-- Legacy Handshake packet constant. This is what we send when we get a legacy
+-- ping from a client.
+legacyHandshakePongConstant :: BS.ByteString
+legacyHandshakePongConstant = BS.pack [0xFF,0x00,0x1b,0x00,0xa7
+  ,0x00,0x31 -- 1
+  ,0x00,0x00 -- Seperator
+  ,0x00,0x33 -- 3 -- Protocol Version number. TODO: Update this with protocolVersion
+  ,0x00,0x31 -- 1
+  ,0x00,0x36 -- 6
+  ,0x00,0x00 -- Seperator
+  ,0x00,0x31 -- 1 -- MC Version
+  ,0x00,0x2e -- .
+  ,0x00,0x31 -- 1
+  ,0x00,0x31 -- 1
+  ,0x00,0x2e -- .
+  ,0x00,0x32 -- 2
+  ,0x00,0x00 -- Seperator
+  ,0x00,0x43 -- C -- Server Name
+  ,0x00,0x69 -- i
+  ,0x00,0x76 -- v
+  ,0x00,0x73 -- s
+  ,0x00,0x6b -- k
+  ,0x00,0x65 -- e
+  ,0x00,0x6c -- l
+  ,0x00,0x6c -- l
+  ,0x00,0x00 -- Seperator
+  ,0x00,0x00 -- 0
+  ,0x00,0x00 -- Seperator
+  ,0x00,0x00 -- 0
+  ]
+
+-- First part of the legacy client ping packet
+legacyHandshakePingConstant :: BS.ByteString
+legacyHandshakePingConstant = BS.pack
+  [0xFE -- SLP PktId
+  ,0x01 -- SLP "payload"
+  ,0xFA -- Plugin Message
+  ,0x00,0x0B -- Length of "MC|PingHost"
+  ,0x00,0x4D -- M
+  ,0x00,0x43 -- C
+  ,0x00,0x7C -- |
+  ,0x00,0x50 -- P
+  ,0x00,0x69 -- i
+  ,0x00,0x6E -- n
+  ,0x00,0x67 -- g
+  ,0x00,0x48 -- H
+  ,0x00,0x6F -- o
+  ,0x00,0x73 -- s
+  ,0x00,0x74 -- t
+  ]
+
+-- A LegacyString is a String that is used in a Legacy Handshake packet
+type LegacyString = BS.ByteString
+
+{-
+ - This instance is deprecated in favor of just storing the 0x00's in with all
+ - the other data. We only use LegacyString like twice, so this is reasonable.
+instance Serial LegacyString where
+  serialize (LegacyString bs) = BS.cons 0x00 (intersperse 0x00 bs)
+  deserialize = error "Undefined: deserialize @LegacyString"
+-}
+
+-- A Minecraft "Short" is a Haskell Int16
 type Short = Int16
+
+-- A Nibble is a Word8 that we promise (not enforced!!!) to only use the 4 least
+-- significant bits.
+-- TODO: Investigate `(Bool,Bool,Bool,Bool)` or `Vect 4 Bool` as alternative.
 type Nibble = Word8
--- Cipher, [Enc|Dec]
+
+-- An EncrptionCouplet holds the current internal state of the encryption
+-- mechanism, including both directions' shift buffers. (Cipher,Enc,Dec)
 type EncryptionCouplet = (AES128, BS.ByteString, BS.ByteString)
--- `PerformsIO` effect
+
+-- Verbify this to match all our other type synonyms like `HasWorld`
 type PerformsIO r = Member IO r
+
+-- Removed in favor of using send directly:
 -- You make an effect that `PerformsIO` by `send`ing an IO action (`send (putStrLn "meow")`)
+-- liftIO :: PerformsIO r => IO a -> Eff r a
+-- liftIO = send
+
+-- TODO: Investigate using a library to provide this type for seperation of concerns
+-- There is no native Word128 type, so we role our own here.
 newtype UUID = UUID (Word64,Word64)
 
+-- This show instance should match the standard Minecraft display format.
 instance Show UUID where
   show (UUID (ua,ub)) = reformat $ showHex ua (showHex ub "")
     where
@@ -128,22 +208,34 @@ instance Show UUID where
       -- Reformat the uuid to what the client expects
       reformat = ins 8 . ins 12 . ins 16 . ins 20
 
+-- A UUID is pretending to be a Word128, so just smoosh all the bits together
 instance Serialize UUID where
   serialize (UUID (a,b)) = serialize a <> serialize b
 
--- Simple way to inject a text message into a json chat string
+-- Simple way to inject a text message into a json chat string. No additional
+-- formatting or checking is done, just raw text.
 jsonyText :: String -> String
 jsonyText s = "{\"text\":\"" ++ s ++ "\"}"
 
--- Datatypes
--- Used in PlayerInfo to track the breaking stage of a block the player is mining
-data BlockBreak = InProgress Word8 | DoneBreaking
+-- Used in PlayerInfo to track the breaking stage of a block the player is mining.
+data BlockBreak 
+  = InProgress Word8 -- The block is still being broken, and has been for this many ticks.
+  | DoneBreaking -- The block is done breaking and should be removed, and this entry removed.
 
--- A block coordinate, not an entity position. Ord is derivied for use in maps.
-data BlockLocation (r :: Relativity) = BlockLocation (Int,Int,Int) deriving (Eq,Ord)
+-- A block coordinate, not an entity position. Ord is derivied for use in maps,
+-- but does not represent any specific concrete ordering, and should not be
+-- relied on for storage etc.
+data BlockLocation (r :: Relativity)
+  = BlockLocation (Int,Int,Int) -- (x,y,z)
+  deriving (Eq,Ord)
+
+-- BlockLocations have some weakish dimensionality. `Absolute` means that it is
+-- an absolute coord relative to (0,0,0). `Relative` means that it is an offset,
+-- often, but not always, relative to a chunk's local (min x,min y = 0, min z)
 type BlockCoord = BlockLocation 'Absolute
 type BlockOffset = BlockLocation 'Relative
 
+-- Have different Show instances to respect the difference as well.
 instance Show BlockCoord where
   show (BlockLocation (x,y,z)) = "(Block)<" ++ show x ++ "," ++ show y ++ "," ++ show z ++ ">"
 
@@ -151,27 +243,46 @@ instance Show BlockOffset where
   show (BlockLocation (x,y,z)) = "(Offset)<" ++ show x ++ "," ++ show y ++ "," ++ show z ++ ">"
 
 {- This is a bad instance that took almost an hour to debug. It still isn't fixed, just worked around (it's derived, and we don't rely on its ordering in Map.elems)
+ - QuickCheck your instances, kids!
 instance Ord BlockCoord where
   compare a@(BlockCoord (xa,ya,za)) b@(BlockCoord (xb,yb,zb)) = if a == b then EQ else if yb > ya then GT else if zb > za then GT else if xb > xa then GT else LT
 -}
 
+-- Only absolute coords can be serialized. This uses Minecraft's efficient
+-- packing of coords into bits of an Int64 because the Y coord is very limited
+-- in range compared to X and Z.
 instance Serialize BlockCoord where
   serialize (BlockLocation (x,y,z)) = serialize $
+    -- 26 MSB
     (shiftL (u x .&. 0x3FFFFFF) 38) .|.
+    -- 12 middle bits
     (shiftL (u y .&. 0xFFF) 26) .|.
+    -- 26 LSB
     (u z .&. 0x3FFFFFF)
     where
       u = unsafeCoerce :: Int -> Int64
 
+-- A Chunk Coord is a coordinate *of* a chunk slice, not *in* a chunk slice.
+-- TODO: Investigate not using chunks at all, and only serializing to chunks on
+-- demand. This can probably be very lazy and very efficient. Maybe even include
+-- lazy chunk generation!
 newtype ChunkCoord = ChunkCoord (Int,Int,Int) deriving (Eq,Ord)
 
+-- This mirrors the format of `Show BlockLocation`
 instance Show ChunkCoord where
   show (ChunkCoord (x,y,z)) = "(Chunk)<" ++ show x ++ "," ++ show y ++ "," ++ show z ++ ">"
 
--- VarInt's max size is an Int32, so we use that to store it. Note that they are also serialized different than a normal MCInt
-newtype VarInt = VarInt {unVarInt :: Int32} deriving (Bits,Enum,Eq,Integral,Num,Ord,Real)
-instance Show VarInt where show = show . unVarInt
+-- VarInt's max size is an Int32, so we use that to store it. 
+-- Note that they are serialized differently from a normal Minecraft Int (which
+-- is a normal Haskell Int32), so we need a newtype. All instances are GND
+newtype VarInt = VarInt {unVarInt :: Int32} deriving (Show,Bits,Enum,Eq,Integral,Num,Ord,Real)
 
+-- VarInt format in binary is:
+--   
+--   iddddddd iddddddd ...
+-- 
+-- Where when `i` is set, there is still another byte to be read. All the `d`s
+-- from all the bytes are concatenated without spacing to form the actual number.
 instance Serialize VarInt where
   serialize n = if moreAfter
     -- If there are more, set the msb and recurse
@@ -184,25 +295,40 @@ instance Serialize VarInt where
       -- Are there more bytes to add?
       moreAfter = shiftR n 7 /= 0
 
-newtype EntityId = EntityId {unEID :: VarInt} deriving (Bits,Enum,Eq,Integral,Num,Ord,Real)
+-- EntityId's are distinguished VarInt's. Note that the `Serial` instance here is
+-- GND, not Generics. (although it could be, and it would work the same)
+newtype EntityId = EntityId {unEID :: VarInt} deriving (Serial,Bits,Enum,Eq,Integral,Num,Ord,Real)
 instance Show EntityId where show = (\p -> "Entity Id {" ++ p ++ "}") . show . unEID
-instance Serialize EntityId where serialize = serialize . unEID
 
-newtype WindowId = WindowId {unWID :: Word8} deriving (Bits,Enum,Eq,Integral,Num,Ord,Real)
+-- WindowId's are distinguished Word8's. `Serial` is GND, not Generics.
+newtype WindowId = WindowId {unWID :: Word8} deriving (Serial,Bits,Enum,Eq,Integral,Num,Ord,Real)
 instance Show WindowId where show = (\p -> "Window Id {" ++ p ++ "}") . show . unWID
-instance Serialize WindowId where serialize = serialize . unWID
 
+-- PlayerId's are *undistinguished* EntityId's because they are not different in
+-- any way except the probably represent players instead of mobs. This is made
+-- explicit to prevent additional confusion.
 type PlayerId = EntityId
 
+-- These are undistinguished because they are used infrequently. 
+-- TODO: Investigate if these should actually be newtypes
 type TransactionId = Short
 type KeepAliveId = VarInt
 type TPConfirmId = VarInt
 
+-- Slots are refactored to this on merge:
+-- A SlotData is a set of information about a specific non-empty Minecraft Slot.
+-- A Slot is a potentially empty Minecraft Slot.
+
 -- Item id, count, damage
 data Slot = Slot (Some Item) Word8
 
+-- An inventory is a mapping from Slot numbers to Slot data (or lack thereof).
+-- TODO: Reconsider sparsity obligations of Map here, and decide on a better
+-- internal representation. Potentially make this abstract after all. There
+-- might be a really fitting purely functional data structure to show off here.
 type Inventory = Map Short Slot
 
+-- Abstraction methods for Inventory, not really needed tbh.
 getSlot :: Short -> Inventory -> Maybe Slot
 getSlot = Map.lookup
 
@@ -210,12 +336,21 @@ setSlot :: Short -> Maybe Slot -> Inventory -> Inventory
 setSlot i Nothing = Map.delete i
 setSlot i (Just x) = Map.insert i x
 
+-- Sidestep existential nonsense when creating items
 slot :: Item i => i -> Word8 -> Slot
 slot i c = Slot (some i) c
 
+-- This is a rather sketchy instance because itemId uniqueness is not enforced
+-- at type level, so we could actually get two different Item instances claiming
+-- the same id, meta, and NBT, which would make this `Eq` instance unreliable.
+-- In fact, it might be *impossible* to write an `Eq` instance for Slots
+-- involving general, open typeclass `Item`s because that would involve deciding
+-- equality for a `forall a. c a => p a` sort of thing, which would only be
+-- based on the `c` instance.
 instance Eq Slot where
   (Slot (SuchThat (Identity (a :: at))) ac) == (Slot (SuchThat (Identity (b :: bt))) bc) = itemId @at == itemId @bt && itemMeta a == itemMeta b && itemNBT a == itemNBT b && ac == bc
 
+-- Extract the item count from a Slot
 slotAmount :: Slot -> Word8
 slotAmount (Slot _ c) = c
 
@@ -232,6 +367,8 @@ splitStack c s@(Just (Slot i _)) = (firstStack,secondStack)
     firstStack = if amtFirstStack == 0 then Nothing else Just $ Slot i amtFirstStack
     (amtFirstStack,secondStack) = removeCount c s
 
+-- Pretty print a slot. Perhaps we should let `Item`s provide a fancy name for
+-- this instance to use.
 instance Show Slot where
   show (Slot (SuchThat (Identity (i :: it))) count) = "{" ++ show count ++ " of [" ++ show (itemId @it) ++ ":" ++ show (itemMeta i) ++ "]" ++ n (itemNBT i) ++ "}"
     where
@@ -239,27 +376,35 @@ instance Show Slot where
       n Nothing = ""
       n (Just nbt) = " with tags: " ++ show nbt
 
+-- Removed on merge
 instance Serialize (Maybe Slot) where
   serialize Nothing = serialize (-1 :: Short)
   serialize (Just (Slot (SuchThat (Identity (i :: it))) count)) = serialize (itemId @it) <> serialize count <> serialize (itemMeta i) <> (case itemNBT i of {Just nbt -> serialize (NBT "" nbt);Nothing -> BS.singleton 0x00})
 
 class Window w where
+  -- Chest
   windowName :: String
+  -- minecraft:chest
   windowIdentifier :: String
+  -- 27
   slotCount :: Short
   -- Bool is transaction success
   onWindowClick :: (HasWorld r,SendsPackets r,Logs r,HasPlayer r) => w -> WindowId -> Short -> TransactionId -> InventoryClickMode -> Eff r Bool
   --clientToCivskellSlot :: Short -> Short
   --civskellToClientSlot :: Short -> Short
 
+-- TODO: Reconsider sparsity obligations. This is pretty dumb in terms of
+-- invariants we must enforce.
 -- Maps coords to non-air blocks. BlockState 0 0 doesn't mean anything. Air just doesn't have anything in the map.
 data ChunkSection = ChunkSection (Map BlockOffset (Some Block))
 
--- Derived instance is really long
+-- Derived instance is really long (it shows all the associations of coords and
+-- blocks in a giant list), so we replace it with a short placeholder
 instance Show ChunkSection where
   show _ = "<Chunk Section>"
 
 -- TODO: Add paletteing. Right now, we send a full 13 bits for *every block*
+-- On second thought, we should rewrite this cancer with the `Data.Bytes` update
 instance Serialize ChunkSection where
   serialize (ChunkSection bs) = serialize bitsPerBlock <> palette <> chunkData <> lights <> lights
     where
@@ -305,6 +450,7 @@ instance Serialize ChunkSection where
       aBlock :: Block b => b -> BB.BitBuilder
       aBlock (b :: bt) = BB.fromBits 9 (blockId @bt) `BB.append` BB.fromBits 4 (blockMeta b)
 
+-- Auth infrastructure is scary, I hope it doesn't break ever :3
 -- Parsed form of the result of authenticating a player with Mojang
 data AuthPacket = AuthPacket UUID String [AuthProperty]
 
@@ -720,7 +866,8 @@ class Packet p => HandledPacket p where
   -- No default defn. here so that we get compiler warnings for not implmenting
   --onPacket p = send (LogString ErrorLog $ "Unsupported packet: " ++ showPacket p)
   -- Parse a packet
-  parsePacket :: Parser p
+  --parsePacket :: Parser p
+  -- Deprecated for Serial
   -- Can `onPacket` possibly poke the packet state, so we need to wait for it to
   -- finish in serial, not parallel?
   canPokePacketState :: Bool
@@ -732,7 +879,9 @@ instance (HandledPacket p,PacketState p ~ s) => HP s p where {}
 class (Packet p,s ~ PacketState p) => PacketWithState s p | p -> s where {}
 instance (Packet p,s ~ PacketState p) => PacketWithState s p where {}
 
--- We can't make this an actual `Show` instance without icky extentions
+-- We can't make this an actual `Show` instance without icky extentions. Also,
+-- the instance context is not checked during instance selection, so we would
+-- have to make a full on `instance Show a where`, which is obviously bad.
 showPacket :: forall p. Packet p => p -> String
 showPacket pkt = formatPacket (packetName @p) (packetPretty pkt)
 
